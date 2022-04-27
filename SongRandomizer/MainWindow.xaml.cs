0760ea00ee8580d6b258a26e76b423a4ddf91d86
Valerii <s20970@pjwstk.edu.pl>
2022-04-27 15:54:35 +0200

    Implemented verification of the textbox input
    TextBox is now verified by regex for the correct input(numbers only)

--- SongRandomizer/MainWindow.xaml.cs
+++ SongRandomizer/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -36,6 +37,12 @@ namespace SongRandomizer
         private async Task LoadWord()
         {
 
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
